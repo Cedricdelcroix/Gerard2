38ce48a0c1eff497ca66877b1b751f8158c2088f
Cedricdelcroix <delcroix.cedric62@gmail.com>
2018-09-21 11:48:59 +0200

    Update

--- ConsoleApp1/Launcher.cs
+++ ConsoleApp1/Launcher.cs
@@ -14,8 +14,8 @@ public class Launcher
     int count = 0;
 
     public Launcher(IWebDriver b)
-	{
-        this.browser = b;        
+    {
+        this.browser = b;
     }
 
     public void run()
@@ -24,8 +24,8 @@ public class Launcher
 
         browser.Navigate().GoToUrl("https://www.easports.com/fr/fifa/ultimate-team/web-app/");
         Console.WriteLine("End goto");
-        //IWebElement header = browser.FindElement(By.Id("site - header"));
-        // Login("delcroix.cedric62@gmail.com","Cedric62100");
+        //Login("delcroix.cedric62@gmail.com","Cedric62100");
+        Thread.Sleep(13000);
         foreach (Player player in playerList)
         {
             GotoMarket();
@@ -48,20 +48,21 @@ public class Launcher
 
     public void initPlayerList()
     {
-        playerList.Add(new Player("", 0));
-        playerList.Add(new Player("", 0));
-        playerList.Add(new Player("", 0));
-        playerList.Add(new Player("", 0));
+        //playerList.Add(new Player("Alex Oxlade-Chamberlain", 2800));
+        playerList.Add(new Player("lemina", 500));
+        //playerList.Add(new Player("Williams", 4700));
+        /*playerList.Add(new Player("", 0));
+        playerList.Add(new Player("", 0));*/
     }
 
     public void GotoMarket()
     {
-        Thread.Sleep(10000);
+        Thread.Sleep(300);
         IWebElement transferButton = browser.FindElement(By.XPath("//button[text() = 'Transferts']"));
         transferButton.Click();
-        Thread.Sleep(500);
+        Thread.Sleep(300);
         IWebElement marketPlace = browser.FindElement(By.ClassName("ut-tile-transfer-market"));
-        Thread.Sleep(500);
+        Thread.Sleep(300);
         marketPlace.Click();
         Console.WriteLine("On market");
     }
@@ -69,33 +70,43 @@ public class Launcher
     public void FindPlayer(Player p)
     {
         IWebElement playerInput = browser.FindElement(By.XPath("//input[@placeholder='Saisissez nom joueur']"));
+        playerInput.Clear();
+        Thread.Sleep(300);
         playerInput.SendKeys(p.name);
+        Thread.Sleep(500);
+        IWebElement playerselect = browser.FindElement(By.XPath("//ul[contains(@class,'playerResultsList')]/button"));
+        playerselect.Click();
         SetPrice(p);
+        Thread.Sleep(500);
+        IWebElement searchButton = browser.FindElement(By.XPath("//button[contains(text(),'Rechercher')]"));
+        searchButton.Click();
         tryBuyPlayer(p);
     }
 
     public void SetPrice(Player p)
     {
-        IWebElement achatImmediat = browser.FindElement(By.XPath("//h1[text() ='Prix achat immédiat :']"));
-        IWebElement Max = achatImmediat.FindElement(By.XPath("//following-sibling::span[text() = 'Max']"));
-        IWebElement inputPrice = Max.FindElement(By.XPath("/following-sibling::input[@placeholder='Tout']"));
-        
+        Thread.Sleep(300);
+        IWebElement element = browser.FindElement(By.XPath("//h1[contains(text(), 'Prix achat immédiat')]/parent::div/following-sibling::div/div/span[contains(text(), 'Max')]/parent::div/following-sibling::div/div/input"));
+        element.Clear();
+        Thread.Sleep(300);
+        element.SendKeys(p.price.ToString());
     }
 
     public bool IsPlayerFind(Player p)
     {
-       
+
         // si le joueur est trouver essayer de l'acheter 
         try
         {
-            //browser.FindElement(By.CssSelector).isDisplayed();
-            Console.WriteLine(p.name + " find");
-            return true;
+            Thread.Sleep(500);
+            browser.FindElement(By.XPath("//span[contains(@class,'no-results-icon')]"));
+            return false;
         }
         catch (OpenQA.Selenium.NoSuchElementException)
         {
-           return false;
-        }        
+            Console.WriteLine(p.name + " find");
+            return true;
+        }
     }
 
     public void tryBuyPlayer(Player p)
@@ -107,16 +118,38 @@ public class Launcher
         }
         else
         {
-            if (count < 15)
-            Thread.Sleep(60000);
-            tryBuyPlayer(p);
+            if (count > 13)
+            {
+                Thread.Sleep(60000);
+                count = 0;
+            }
+            else
+            {
+                count++;
+            }
+            RetryBuyPlayer(p);
         }
+    }
+
+    public void RetryBuyPlayer(Player p)
+    {
+        //GotoMarket();
+        IWebElement element = browser.FindElement(By.XPath("//button[contains(@class,'btn-navigation')][1]"));
+        element.Click();
+        Thread.Sleep(300);
+        IWebElement searchButton = browser.FindElement(By.XPath("//button[contains(text(),'Rechercher')]"));
+        searchButton.Click();
+        tryBuyPlayer(p);
     }
 
     public void BuyPlayer()
     {
         // click on acheter 
-
+        IWebElement offreButton = browser.FindElement(By.XPath("//button[contains(text(),'Achat immédiat')]"));
+        offreButton.Click();
+        Thread.Sleep(300);
+        IWebElement confirmeoffreButton = browser.FindElement(By.XPath("//button[contains(text(),'Ok')]"));
+        confirmeoffreButton.Click();
         Console.WriteLine("Player buy");
     }
 
